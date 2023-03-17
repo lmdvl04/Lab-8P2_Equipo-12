2de3a88a52aedd71926d7a3be24c2a7cb42c5655
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-03-17 14:23:20 -0600

    este si

---
+++ src/lab/pkg8p2_equipo/pkg12/Lab8P2_Equipo12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg8p2_equipo.pkg12;
+
+/**
+ *
+ * @author HTS
+ */
+public class Lab8P2_Equipo12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
